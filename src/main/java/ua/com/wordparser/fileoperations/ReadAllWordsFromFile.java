55d3cf05d66d1723efc37703d5dabc3a9b763cbf
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-11 22:21:54 +0300

    operations in file with words

---
+++ src/main/java/ua/com/wordparser/fileoperations/ReadAllWordsFromFile.java
@@ -0,0 +1,58 @@
+package ua.com.wordparser.fileoperations;
+
+import lombok.SneakyThrows;
+import org.apache.poi.POITextExtractor;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class ReadAllWordsFromFile {
+    public static long getWordCount(String file) throws IOException {
+        POITextExtractor textExtractor;
+        XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
+        textExtractor = new XWPFWordExtractor(doc);
+        return Arrays.stream(textExtractor.getText().split("\\s+"))
+                .filter(s -> s.matches("^.*[\\p{L}\\p{N}].*$"))
+                .count();
+    }
+
+    @SneakyThrows
+    public static List<String> getWordSizeLargerThanNumber(String file, int shortLength) {
+        POITextExtractor textExtractor;
+        XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
+        textExtractor = new XWPFWordExtractor(doc);
+        return Arrays.stream(textExtractor.getText().split("\\s+"))
+                .filter(s -> s.length() > shortLength)
+                .collect(Collectors.toList());
+    }
+
+    @SneakyThrows
+    public static List<String> getMostPopularWords(String file, int quantity) {
+        POITextExtractor textExtractor;
+        XWPFDocument doc = new XWPFDocument(new FileInputStream(file));
+        textExtractor = new XWPFWordExtractor(doc);
+
+        List<String> text = Arrays.asList((textExtractor.getText().split("\\s+")));
+        Map<String, Integer> map = new HashMap<>();
+        text.forEach(s -> {
+            if (map.containsKey(s)) {
+                int value = map.get(s);
+                map.put(s, value + 1);
+            } else {
+                map.put(s, 1);
+            }
+        });
+        List<Word> list = new ArrayList<>();
+        map.forEach((s, integer) -> list.add(new Word(s, integer)));
+        list.sort(Comparator.comparing(Word::getCount).reversed());
+        return list.stream()
+                .map(Word::getWord)
+                .limit(quantity)
+                .collect(Collectors.toList());
+    }
+}
